debc8240acd7018ee4361bc6fdcf8af75bc0851e
wpj09 <wpjwpj09@gmail.com>
2023-01-01 15:11:30 -0300

    Helper de menu ativo

---
+++ app/Helpers/isActive.php
@@ -0,0 +1,8 @@
+<?php
+if (!function_exists('isActive')) {
+
+    function isActive($href, $class = 'active')
+    {
+        return $class = (strpos(Route::currentRouteName(), $href) === 0 ? $class : '');
+    }
+}
